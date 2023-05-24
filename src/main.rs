ed1ccecd7aa2204ee2871517884eb91f29f18518
bahdotsh <appu.yess@gmail.com>
2023-05-24 12:22:30 +0530

    tree struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::{cell::RefCell, rc::Rc};
+
+#[derive(Debug, Clone)]
+pub struct TreeNode {
+    val: i32,
+    left: Option<TreeNodeRef>,
+    right: Option<TreeNodeRef>,
+}
+
+type TreeNodeRef = Rc<RefCell<TreeNode>>;
+
 fn main() {
     println!("Hello, world!");
+
 }
